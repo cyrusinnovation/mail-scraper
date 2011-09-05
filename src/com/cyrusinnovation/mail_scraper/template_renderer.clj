2ba792a6dff12a7b93cd1f5f710923dd7b97c827
Paul Blair <ciriwe@phobot.net>
2011-09-05 16:11:10 -0400

    Deleted redundant report.jsp; refactored template preparation macro to allow passing in the signature and the mappings.

--- src/com/cyrusinnovation/mail_scraper/template_renderer.clj
+++ src/com/cyrusinnovation/mail_scraper/template_renderer.clj
@@ -1,8 +1,28 @@
 (ns com.cyrusinnovation.mail-scraper.template-renderer
+	(:require [net.cgrand.enlive-html :as html] [clojure.string :as str])
 	(:gen-class :extends javax.servlet.http.HttpServlet))
 
-;; Where is it documented that you override a method by
-;; preceding its name with a hyphen? Or that the object
-;; is the first argument?
+(import javax.servlet.http.HttpServletResponse)
+
+(defmacro prepare-template [this template-name template-path template-signature & template-mapping-forms]
+	`(html/deftemplate
+		 ~(symbol template-name)
+		 (new java.io.File (.getRealPath (.getServletContext ~this) ~template-path))
+		 ~template-signature
+		 ~@template-mapping-forms))
+
+(defn render-template [this template-name]
+	(let [template-path (str  "/WEB-INF/templates/" template-name ".html")
+				template (prepare-template this template-name template-path
+																	 [substitution-values]
+																	 [:h2.eventTitle] (html/content (:message substitution-values)))]
+        (apply str (template {:message "There are no networking events at this time."}))))
+
+(defn set-body [response html]
+    (let [writer (.getWriter response)]
+        (.write writer html)))
+
 (defn -service [this request response]
-	(.sendRedirect response "/report.jsp"))
+	(let [template-name (str/replace (.getServletPath request) "/" "")]
+		(set-body response (render-template this template-name))
+		(.setStatus response HttpServletResponse/SC_OK)))
--- war/report.jsp
+++
@@ -1,7 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head><title>Networking Events</title></head>
-  <body>
-		<h2 class="eventTitle">There are no networking events at this time.</h2>
-	</body>
-</html>
