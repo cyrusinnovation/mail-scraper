2797f3b4c5673e1a2c8011fd4181409b4d7435b6
Matt Fowler <matt.fow@gmail.com>
2011-09-09 11:53:17 -0400

    Fixed broken parens in test

--- test/end_to_end_tests.clj
+++ test/end_to_end_tests.clj
@@ -21,8 +21,8 @@
 (fact "The application persists and retrieves an event"
   (let [timestamp (str (new java.util.Date))]
 			(.get driver (str "http://localhost:8080/events?name=An%20Event%20with%20a%20%3Ctag%3E%20at%20" timestamp))
-			(-> (.findElement driver (By/tagName "title")) (.getText)) => "Submitted")
+			(-> (.findElement driver (By/tagName "title")) (.getText)) => "Submitted"
 			(.get driver "http://localhost:8080/report")
-			(-> (.findElement driver (By/cssSelector ".eventTitle")) (.getText)) => (str "An Event with a <tag> at " timestamp))
+			(-> (.findElement driver (By/cssSelector ".eventTitle")) (.getText)) => (str "An Event with a <tag> at " timestamp)))
 
 (.quit driver))
