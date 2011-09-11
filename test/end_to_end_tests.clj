710f7586e783aadfc857aa51e23a3b9da1bcecce
Paul Blair <ciriwe@phobot.net>
2011-09-11 03:14:19 -0400

    Use the admin console to delete the data before each test so there are no cross-test dependencies. Add code to make it possible to do POST requests in future tests. Remove the appengine_clj test utils which are only useful apparently for local tests -- and don't work. There are more efficient ways to set up local persistence tests against the app engine data store now.

--- test/end_to_end_tests.clj
+++ test/end_to_end_tests.clj
@@ -4,6 +4,49 @@
 (import org.openqa.selenium.firefox.FirefoxDriver)
 (import org.openqa.selenium.By)
 
+(import org.apache.http.impl.client.DefaultHttpClient)
+(import org.apache.http.client.methods.HttpPost)
+(import '(org.apache.http HttpEntity NameValuePair HttpResponse))
+(import org.apache.http.message.BasicNameValuePair)
+(import org.apache.http.client.entity.UrlEncodedFormEntity)
+(import org.apache.http.util.EntityUtils)
+
+; This only works for local persistence tests.
+;; (import '(com.google.appengine.tools.development.testing
+;;           LocalDatastoreServiceTestConfig
+;;           LocalServiceTestHelper))
+;; (background (around :facts (let [config (new LocalDatastoreServiceTestConfig)
+;;                                  helper (new LocalServiceTestHelper
+;;                                              (into-array LocalDatastoreServiceTestConfig [config]))]
+;;                              (.setUp helper) ?form (.tearDown helper))))
+
+
+;; This apparently only works on the python server
+;; (background (before :facts (post "http://localhost:8080/_ah/admin/interactive/execute"
+;;                                  {"code" "from google.appengine.ext import db\ndb.delete(db.Query())"})))
+
+
+(defn http-execute [client method]
+  (let [response (.execute client method)]
+    (.getEntity response)))
+
+(defn stringify-response [response-entity]
+  (cond
+   (nil? response-entity) ""
+   (> 500000 (.getContentLength response-entity)) (throw (new RuntimeException "Content length too large."))
+   :else (EntityUtils/toString response-entity)))
+  
+(defn post [url key-value-pairs]
+  (let [client (new DefaultHttpClient)
+        method (new HttpPost url)
+        request-params (map (fn [pair] (new BasicNameValuePair (pair 0) (pair 1)))
+                            (seq key-value-pairs))]
+    (.setEntity method (new UrlEncodedFormEntity request-params "UTF-8"))
+    (let [response (http-execute client method)]
+      (try (stringify-response response)
+           (finally (EntityUtils/consume response))))))
+
+
 ;; Why isn't this working?
 ;; (background (around :contents (let [driver (new FirefoxDriver)]
 ;; 																?form
@@ -11,6 +54,14 @@
 
 (let [driver (new FirefoxDriver)]
 
+  (background (before :facts (try
+                               (do
+                                 (.get driver "http://localhost:8080/_ah/admin/datastore?kind=Event")
+                                 (-> (.findElement driver (By/id "allkeys")) (.click))
+                                 (-> (.findElement driver (By/id "delete_button")) (.click))
+                                 (-> (.switchTo driver) (.alert) (.accept)))
+                               (catch Throwable t :no-data-to-clear))))
+                      
 ;; Need to empty the database before these tests run
 (fact "Page with no events results from empty database"
 			(.get driver "http://localhost:8080/report")
